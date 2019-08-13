18c010ca4d15f6442a02a83d2aca8fb8b2a48449
nori266 <noriel266@gmail.com>
2019-08-13 18:24:04 +0700

    fixed streaming error by adding sleep after SluRequest

--- python/simple.py
+++ python/simple.py
@@ -1,5 +1,6 @@
 import os
 import wave
+import time
 
 import grpc
 
@@ -9,19 +10,31 @@ from sg_pb2_grpc import SluStub as SLUService
 
 chunk_size = 8000
 
+sample_rate_hertz=16000
+
+
+
 if 'APP_ID' not in os.environ:
     raise RuntimeError('APP_ID environment variable needs to be set')
 
+FILE_NAME = '../output45.wav'
+
 
 def audio_iterator():
-    yield SluRequest(config=SluConfig(channels=1, sample_rate_hertz=16000))
+    yield SluRequest(config=SluConfig(channels=1, sample_rate_hertz=sample_rate_hertz, language_code='en-US'))
     yield SluRequest(event=SluEvent(event='START'))
-    with wave.open('../audio.wav', mode='r') as audio_file:
+    with wave.open(FILE_NAME, mode='r') as audio_file:
         audio_bytes = audio_file.readframes(chunk_size)
+
         while audio_bytes:
+
             yield SluRequest(audio=audio_bytes)
             audio_bytes = audio_file.readframes(chunk_size)
+            time.sleep(float(chunk_size) / sample_rate_hertz)
+
     yield SluRequest(event=SluEvent(event='STOP'))
+
+
 
 
 with grpc.secure_channel('api.speechgrinder.com', grpc.ssl_channel_credentials()) as channel:
